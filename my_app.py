b90e29892a9712f1795d0da6ce6130d60218be88
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-20 12:02:40 +0100

    Fix python version 3.6

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ import simplejson
 
 ver = sys.version_info
 print("python version is", ver)
-if not (ver.major == 3 and ver.minor == 7):
+if not (ver.major == 3 and ver.minor == 6):
   exit(1)
 print(os.getenv('TEST_NUMBER'))
 
