0373c0bfaf92a9184f6d44d77d6df07ce00a5e9e
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-20 14:32:03 +0100

    Build for linux and osx

--- conda.recipe/build_package.sh
+++ conda.recipe/build_package.sh
@@ -5,5 +5,5 @@ BUILD_PATH=$HOME/conda-build
 mkdir -p $BUILD_PATH
 conda config --set anaconda_upload no
 conda build --output-folder $BUILD_PATH ./conda.recipe/
-anaconda -t $ANACONDA_TOKEN upload -u $ANACONDA_USER --force $BUILD_PATH/noarch/my_app-1.0-py_0.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload -u $ANACONDA_USER --force $BUILD_PATH/${TRAVIS_OS_NAME}-64/my_app-1.0-py36_0.tar.bz2
 
