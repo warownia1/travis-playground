192d6aefa2cb3e108901abb33df1ffadaac769da
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-20 15:23:16 +0100

    Simplify recipe and set auto-upload

--- conda.recipe/build_package.sh
+++ conda.recipe/build_package.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-BUILD_PATH=$HOME/conda-build
-mkdir -p $BUILD_PATH
-conda config --set anaconda_upload no
-conda build --output-folder $BUILD_PATH ./conda.recipe/
-anaconda -t $ANACONDA_TOKEN upload -u $ANACONDA_USER --force $BUILD_PATH/${TRAVIS_OS_NAME}-64/my_app-1.0-py36_0.tar.bz2
+conda config --set anaconda_upload yes
+conda build --token $ANACONDA_TOKEN --user $ANACONDA_USER \
+  --quiet ./conda.recipe/
 
